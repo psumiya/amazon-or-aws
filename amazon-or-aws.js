16b0b30435b7276c4eabf92e6f754eabb1718121
psumiya <sumiya.p@gmail.com>
2023-01-27 06:49:09 +0530

    Try with new data

--- amazon-or-aws.js
+++ amazon-or-aws.js
@@ -1,21 +1,27 @@
 function onload() {
+    var results = [];
     fetch('service-list.json')
       .then((response) => response.json())
       .then((data) => {          
         var tbodyRef = document.getElementById('resultTable').getElementsByTagName('tbody')[0];
-        data.items.forEach(function (item, index) {
-          if (item && item.additionalFields && item.additionalFields.category && item.additionalFields.category == "products") {              
-            const indexCell = '<th scope="row">' + (index + 1) + '</th>';
-            const name = '<td>' + item.additionalFields.title + '</td>';
-            const desc = '<td>' + item.additionalFields.desc + '</td>';
-            const launchDate = '<td>' + item.dateCreated + '</td>';
-            const link = '<td><a href="' + item.additionalFields.primaryUrl + '">View</a></td>';
+        var i = 0;
+        data.items.forEach(function (entry, index) {
+          const item = entry.item;
+          if (item && item.additionalFields) {
+            i = i + 1;
+            results.push(item);
+            const indexCell = '<th scope="row">' + i + '</th>';
+            const name = '<td>' + item.additionalFields.productName + '</td>';
+            const desc = '<td>' + item.additionalFields.productSummary + '</td>';
+            const launchDate = '<td>' + item.additionalFields.launchDate + '</td>';
+            const link = '<td><a href="' + item.additionalFields.productUrl + '">View</a></td>';
             const contentHtml = indexCell + name + desc + launchDate + link;
             const newRow = tbodyRef.insertRow(tbodyRef.rows.length);
             newRow.innerHTML = contentHtml;
           }
         });      
       });
+      return results;
   }
 
-onload();
+const results = onload();
